520884ae431f9994d8e90b39c87d0c16d2419a6e
Ostefar <56957832+Ostefar@users.noreply.github.com>
2022-05-10 16:03:00 +0200

    231

--- Bogholderen/src/app/users/add-users.component.ts
+++ Bogholderen/src/app/users/add-users.component.ts
@@ -1,8 +1,19 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './add-users.component.html',
 })
-export class AddUsersComponent {
+export class AddUsersComponent implements OnInit
+{
+
+  constructor()
+  {
+  }
+
+  ngOnInit()
+  {
+
+  }
 }

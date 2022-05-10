64c1321bd0e0ba3f0978ea8f98789ed2375139a8
Ostefar <56957832+Ostefar@users.noreply.github.com>
2022-05-10 15:08:05 +0200

    roting

--- Bogholderen/src/app/app.module.ts
+++ Bogholderen/src/app/app.module.ts
@@ -7,17 +7,15 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { NbButtonModule } from '@nebular/theme';
+import { AddUsersComponent } from './users/add-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    AddUsersComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,8 +24,7 @@ import { NbButtonModule } from '@nebular/theme';
     NbButtonModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'users', component: AddUsersComponent },
     ])
   ],
   providers: [],
---
+++ Bogholderen/src/app/users/add-users.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-component',
+  templateUrl: './add-users.component.html',
+})
+export class AddUsersComponent {
+}

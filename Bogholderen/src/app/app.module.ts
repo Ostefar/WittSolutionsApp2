842deb76c574a0504614adae8911584393859c86
Ostefar <56957832+Ostefar@users.noreply.github.com>
2022-05-11 11:11:09 +0200

    up

--- Bogholderen/src/app/app.module.ts
+++ Bogholderen/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { NbButtonModule } from '@nebular/theme';
+import { NbButtonModule, NbTabsetModule } from '@nebular/theme';
 import { AddUsersComponent } from './users/add-users.component';
 
 @NgModule({
@@ -22,6 +22,8 @@ import { AddUsersComponent } from './users/add-users.component';
     HttpClientModule,
     FormsModule,
     NbButtonModule,
+    ReactiveFormsModule,
+    NbTabsetModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'add-users', component: AddUsersComponent },
--- Bogholderen/src/app/users/add-users.component.ts
+++ Bogholderen/src/app/users/add-users.component.ts
@@ -1,9 +1,11 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   templateUrl: './add-users.component.html',
+  styleUrls: ['./add-users.component.css']
 })
 export class AddUsersComponent implements OnInit
 {
@@ -12,8 +14,24 @@ export class AddUsersComponent implements OnInit
   {
   }
 
+
   ngOnInit()
   {
 
+  }
+
+  addUserForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    userName: new FormControl(''),
+    password: new FormControl(''),
+    phone: new FormControl(''),
+    email: new FormControl(''),
+    addressId: new FormControl(''),
+  });
+
+  onSubmit()
+  {
+    console.warn(this.addUserForm.value);
   }
 }

6b77f8092e7ec0b2d5e53a5355b7506d2dbf5bcf
Ostefar <56957832+Ostefar@users.noreply.github.com>
2022-06-04 15:25:34 +0200

    toggle logout btn

--- Bogholderen/src/app/nav-menu/nav-menu.component.ts
+++ Bogholderen/src/app/nav-menu/nav-menu.component.ts
@@ -1,3 +1,4 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
@@ -8,12 +9,18 @@ import { LoginService } from '../service/login-service';
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
   isExpanded = false;
+  isLoggedIn = false;
 
 
 
   constructor(private translate: TranslateService, private loginService: LoginService, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.toggleLogOutBtn();
+
   }
 
   collapse() {
@@ -31,5 +38,12 @@ export class NavMenuComponent {
   logOut(): void {
     this.loginService.logOut();
     this.router.navigateByUrl("/login");
+  }
+  toggleLogOutBtn() { 
+    if (localStorage.getItem("token") != null) {
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+    }
   }
 }
